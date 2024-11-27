2ce970c1235d43ff805f1e6eb4a836785f3d0d56
ItsMyName44 <Noviyashik15@yandex.ru>
2024-11-27 17:29:57 +0300

    sec4mus1 commit

--- backgr.js
+++ backgr.js
@@ -2,66 +2,108 @@ const button0 = document.getElementById('butn0');
 button0.addEventListener('click', function() {
   const body = document.querySelector('body');
   body.style.backgroundImage = 'url(back/iguazu.jpg)';
+  obnulenie2();
+  this.style.color = 'blue';
 });
 const button1 = document.getElementById('butn1');
 button1.addEventListener('click', function() {
   const body = document.querySelector('body');
   body.style.backgroundImage = 'url(back/iguazu0.jpg)';
+  obnulenie2();
+  this.style.color = 'blue';
 });
 const button2 = document.getElementById('butn2');
 button2.addEventListener('click', function() {
   const body = document.querySelector('body');
   body.style.backgroundImage = 'url(back/iguazu1.jpg)';
+  obnulenie2();
+  this.style.color = 'blue';
 });
 const button3 = document.getElementById('butn3');
 button3.addEventListener('click', function() {
   const body = document.querySelector('body');
   body.style.backgroundImage = 'url(back/iguazu2.jpg)';
+  obnulenie2();
+  this.style.color = 'blue';
 });
 const button4 = document.getElementById('butn4');
 button4.addEventListener('click', function() {
   const body = document.querySelector('body');
   body.style.backgroundImage = 'url(back/iguazu3.jpg)';
+  obnulenie2();
+  this.style.color = 'blue';
 });
 const button5 = document.getElementById('butn5');
 button5.addEventListener('click', function() {
   const body = document.querySelector('body');
   body.style.backgroundImage = 'url(back/iguazu4.jpg)';
+  obnulenie2();
+  this.style.color = 'blue';
 });
 const button6 = document.getElementById('butn6');
 button6.addEventListener('click', function() {
   const body = document.querySelector('body');
   body.style.backgroundImage = 'url(back/iguazu5.jpg)';
+  obnulenie2();
+  this.style.color = 'blue';
 });
 const button7 = document.getElementById('butn7');
 button7.addEventListener('click', function() {
   const body = document.querySelector('body');
   body.style.backgroundImage = 'url(back/iguazu6.jpg)';
+  obnulenie2();
+  this.style.color = 'blue';
 });
 const button8 = document.getElementById('butn8');
 button8.addEventListener('click', function() {
   const body = document.querySelector('body');
   body.style.backgroundImage = 'url(back/iguazu7.jpg)';
+  obnulenie2();
+  this.style.color = 'blue';
 });
 const button9 = document.getElementById('butn9');
 button9.addEventListener('click', function() {
   const body = document.querySelector('body');
   body.style.backgroundImage = 'url(back/iguazu8.jpg)';
+  obnulenie2();
+  this.style.color = 'blue';
 });
 const button10 = document.getElementById('butn10');
 button10.addEventListener('click', function() {
   const body = document.querySelector('body');
   body.style.backgroundImage = 'url(back/iguazu9.jpg)';
+  obnulenie2();
+  this.style.color = 'blue';
 });
 const button11 = document.getElementById('butn11');
 button11.addEventListener('click', function() {
   const body = document.querySelector('body');
   body.style.backgroundImage = 'url(back/iguazu10.jpg)';
+  obnulenie2();
+  this.style.color = 'blue';
 });
 const button12 = document.getElementById('butn12');
 button12.addEventListener('click', function() {
   const body = document.querySelector('body');
   body.style.backgroundImage = 'url(back/iguazu11.jpg)';
+  obnulenie2();
+  this.style.color = 'blue';
 });
+
+var obnulenie2 = function(){
+  button0.style.color = 'red';
+  button1.style.color = 'red';
+  button2.style.color = 'red';
+  button3.style.color = 'red';
+  button4.style.color = 'red';
+  button5.style.color = 'red';
+  button6.style.color = 'red';
+  button7.style.color = 'red';
+  button8.style.color = 'red';
+  button9.style.color = 'red';
+  button10.style.color = 'red';
+  button11.style.color = 'red';
+  button12.style.color = 'red';
+}
 
 
